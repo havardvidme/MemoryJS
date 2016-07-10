464aa4178ae7e6398c0fb8bbf0c478d3328e7b46
Håvard Vidme <havard.vidme@gmail.com>
2016-07-10 18:13:33 +0200

    Created the cards with js

--- memory.js
+++ memory.js
@@ -1,2 +1,14 @@
 // Get cards container.
 var cards = document.getElementById('cards');
+
+// Set card values.
+var cardsHorizontal = 4;
+var cardsVertical = 4;
+var cardsTotal = cardsHorizontal * cardsVertical;
+
+// Create each card and add to container.
+for (var cardIndex = 0; cardIndex < cardsTotal; cardIndex++) {
+    var card = document.createElement('div');
+    card.appendChild(document.createTextNode(cardIndex));
+    cards.appendChild(card);
+}
