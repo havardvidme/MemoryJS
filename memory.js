942b04d6781c5d748dee9349169a0dfdc3710ed1
Håvard Vidme <havard.vidme@gmail.com>
2016-07-10 18:25:34 +0200

    Added basic card styles and classes

--- memory.js
+++ memory.js
@@ -6,9 +6,13 @@ var cardsHorizontal = 4;
 var cardsVertical = 4;
 var cardsTotal = cardsHorizontal * cardsVertical;
 
+// Add classes to cards container.
+cards.classList.add('cards', 'cards-horizontal-' + cardsHorizontal.toString(), 'cards-vertical-' + cardsVertical.toString());
+
 // Create each card and add to container.
 for (var cardIndex = 0; cardIndex < cardsTotal; cardIndex++) {
     var card = document.createElement('div');
+    card.classList.add('card');
     card.appendChild(document.createTextNode(cardIndex));
     cards.appendChild(card);
 }
